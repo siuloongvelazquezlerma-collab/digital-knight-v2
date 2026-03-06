e7fde451f36fa1cc94693c8b6ebbe4550210da88
siuloong velazquez <siuloong85@gmail.com>
2026-03-06 00:32:12 -0600

    assetlinks for twa

---
+++ app/src/main/java/app/digitalknight/v2/DelegationService.java
@@ -0,0 +1,14 @@
+package app.digitalknight.v2;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
