dfc535c0e9ab522589cb94c8fd37f78807ee3c43
siuloong velazquez <siuloong85@gmail.com>
2026-07-03 00:14:52 -0600

    update digital knight

--- www/player-series-module.js
+++ www/player-series-module.js
@@ -1,4 +1,4 @@
-import { loadMostRecentProgress, syncData, loadProfileInfo, throttledSyncData } from './js/sync-supabase.js';
+import { loadMostRecentProgress, syncData, loadProfileInfo, throttledSyncData, saveSeriesProgress } from './js/sync-supabase.js';
 import { onPlayEpisode } from './viewsTracker.js';
 import { supabase } from './js/sync-supabase.js';
 
