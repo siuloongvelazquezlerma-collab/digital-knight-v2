3ef5e2e94ada934427e9fcd43e0049dbb325148e
siuloong velazquez <siuloong85@gmail.com>
2026-09-09 06:28:35 -0600

    update digital knight

--- www/script.js
+++ www/script.js
@@ -110,7 +110,7 @@ window.showUpdateModal = createUpdateModal;
 // Solo cambia CURRENT_VERSION cuando haya una nueva actualización
 // El modal aparece automáticamente una vez por versión
 
-const CURRENT_VERSION = "1.3.9";
+const CURRENT_VERSION = "1.3.10";
 
 function checkForUpdateModal() {
     const lastShown = localStorage.getItem("lastUpdateShown");
